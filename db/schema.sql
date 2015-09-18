dd579aceff88390ccfbace82e2897c30732b6463
Bryan-Kirk Reinhardt <bkr@bkrs-MBP-15-work.local>
2015-09-18 14:18:01 -0700

    cleanup

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `coreos_deploy` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `coreos_deploy`;
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
---
--- ------------------------------------------------------
--- Server version	5.6.23-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -75,4 +72,3 @@ CREATE TABLE `deploys` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-27 13:28:11
